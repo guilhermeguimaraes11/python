5d14ddd567d2f505e70ea525932f847535d20ba0
AlunoDS <vinicius.souza.oliveira123@gmail.com>
2024-03-25 14:16:12 -0300

    Exercicios1 e 2 -Operações arit. e PAR

---
+++ exercicio.py
@@ -0,0 +1,15 @@
+import time
+ 
+numero1 = 10
+numero2 = 15
+
+print("Adição:", numero1 + numero2 )
+print("Subtração:",  numero1 - numero2)
+print("Multiplicação:",  numero1 * numero2)
+print("Divisão:", numero1 / numero2  )
+print("\n\nDivisão Inteira:", numero1 // numero2)
+print("\n\nResto da divisão:", numero1 % numero2)
+print("\n\nPotência:", numero1 ** numero2)
+
+time.sleep(5)
+
---
+++ exercicio2.py
@@ -0,0 +1,14 @@
+import time  
+numero = float(input("Digite um número inteiro positivo:"))
+resposta = numero % 2
+
+if(resposta == 0):
+    print('"P-A-R!!!"')
+
+else:
+    print("Tente outra vez!")
+
+
+time.sleep(5)
+ 
+ 
